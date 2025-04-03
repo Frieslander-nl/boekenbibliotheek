7e1b72e3bd287e828fc87b687c7279dfe7c35892
Frieslander-nl <germenpcliezenga@gmail.com>
2025-04-03 09:21:14 +0200

    comment

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> <!--Edit knop toevoegen-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>City Library</title>
